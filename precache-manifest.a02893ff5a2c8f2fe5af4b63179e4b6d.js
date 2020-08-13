b71eade290e0113ae08efbc2424b55a6183d62ee
Luke Sparrow <luke.sparrow@pm.me>
2020-08-13 11:23:55 -0600

    Updates

---
+++ precache-manifest.a02893ff5a2c8f2fe5af4b63179e4b6d.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52f79825fb145b21ee6e6a893c19312d",
+    "url": "/Pokefinder/index.html"
+  },
+  {
+    "revision": "0524587b5ba18498a8ad",
+    "url": "/Pokefinder/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "620a57e0ceabed2faddd",
+    "url": "/Pokefinder/static/css/main.ae7afe6e.chunk.css"
+  },
+  {
+    "revision": "0524587b5ba18498a8ad",
+    "url": "/Pokefinder/static/js/2.5c3320ea.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Pokefinder/static/js/2.5c3320ea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "620a57e0ceabed2faddd",
+    "url": "/Pokefinder/static/js/main.90665b3d.chunk.js"
+  },
+  {
+    "revision": "bfb34b5ae0c0d902a12c",
+    "url": "/Pokefinder/static/js/runtime-main.834b702b.js"
+  },
+  {
+    "revision": "9cf68920f03d0f3a507f972feff9139a",
+    "url": "/Pokefinder/static/media/PokemonSolidNormal.9cf68920.woff2"
+  },
+  {
+    "revision": "d7e60a9c2300e66b6d677ddd5d9ca9a5",
+    "url": "/Pokefinder/static/media/PokemonSolidNormal.d7e60a9c.woff"
+  }
+]);
