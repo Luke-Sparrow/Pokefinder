db54d9f6b3b6d298ba3db8d7c7e993e337e9b1c7
Luke Sparrow <luke.sparrow@pm.me>
2020-08-13 16:09:10 -0600

    updating css

--- src/PokeCard.js
+++ src/PokeCard.js
@@ -10,26 +10,27 @@ class PokeCard extends React.Component {
     }
     componentDidMount() {
         fetch(this.props.url).then(response => response.json())
-        .then( (info) => {
-            this.setState({
-                thisPokemonId: info.id,
-                imgUrl: info.sprites.front_default })
-        })
+            .then((info) => {
+                this.setState({
+                    thisPokemonId: info.id,
+                    imgUrl: info.sprites.front_default
+                })
+            })
 
         // this.setState({imgUrl: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/'+ this.state.thisPokemonId.toString() + '.png' })
     }
 
-    render () {
+    render() {
         const { imgUrl, thisPokemonId } = this.state
-        return (
-            <div className='tc bg-lightest-blue dib br3 pa3 ma2 grow bw2 shadow-5'>
-                <img src={imgUrl} alt="pokemon"/>
+        return ( 
+            <div className='tc dib bg-dark-red br3 pa3 ma2 grow bw2 shadow-5'>
+                <img src={ imgUrl } alt="pokemon" className='bg-lightest-blue'/>
                 <div>
-                    <h1>{this.props.name.charAt(0).toUpperCase() + this.props.name.slice(1)}</h1>
-                    <p>National Dex # {thisPokemonId}</p>
-                </div>
+                    <h1> {this.props.name.charAt(0).toUpperCase() + this.props.name.slice(1)} </h1> 
+                    <p>National Dex# {thisPokemonId} </p> 
+                </div > 
             </div>
-            )
+        )
     }
 }
 
--- src/SearchBox.js
+++ src/SearchBox.js
@@ -1,15 +1,14 @@
 import React from 'react';
 
 
-const SearchBox = ({searchChange}) => {
-    return (
-        <div className='pa2'>
-            <input 
-            type='search' 
-            className='pa3 ba b--blue bg-light-green tc' 
-            placeholder='Enter Pokemon Name!' 
-            onChange={searchChange}
-            />
+const SearchBox = ({ searchChange }) => {
+    return ( 
+        <div className = 'pa2'>
+            <input type = 'search'
+            className = 'pa3 ba b--blue bg-lightest-blue tc'
+            placeholder = 'Enter Pokemon Name!'
+            onChange = { searchChange }
+            /> 
         </div>
     )
 }
